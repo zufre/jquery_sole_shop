628b3bec18417c9eb49e771549954cfaae4d8cdc
Cédric David <cedric.david@btopenworld.com>
2018-11-22 10:31:22 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
 $(document).ready(() => {
-  const $menuButton = $('.menu-button');
-  const $navDropdown = $('#nav-dropdown');
-
-  $menuButton.on('click', () => {
-    $navDropdown.show();
-  })
+  $('.menu-button').on('click',() => {
+    $('#nav-dropdown').slideToggle('slow');
+  });
+	
+  $(".login-button").on("click", () => {
+    $(".login-form").slideToggle();
+  });
   
-  $navDropdown.on('mouseleave', () => {
-    $navDropdown.hide();
-  })
-})
-
-
+  $(".arrow-one").on("click", () => {
+    $(".shoe-details-one").toggle();
+  });
+  
+  $(".sizes-one").on("click", () => {
+    $(".text-one").fadeIn(1000);
+  });
+  
+  $(".arrow-two").on("click", () => {
+    $(".shoe-details-two").toggle();
+  });
+  
+  $(".sizes-two").on("click", () => {
+    $(".text-two").fadeIn();
+  });
+  
+  $(".arrow-three").on("click", () => {
+    $(".shoe-details-three").toggle();
+  });
+  
+  $(".sizes-three").on("click", () => {
+    $(".text-three").fadeIn(1000);
+  });
+  
+});
